4a5f20bb6b650f55eb81bc748f0c3e69981e26be
javierjah <javier.palacios.h@gmail.com>
2017-04-13 21:42:48 -0300

    delete unused props and function and make component behaviour more pure

--- src/container/Calendar.react.js
+++ src/container/Calendar.react.js
@@ -42,25 +42,24 @@ export default class Calendar extends Component {
       stage: props.startStage,
       focus: Moment(props.selected).startOf('month'),
       monthOffset: 0,
-      daysWithEvent: props.annualEvents,
       date: Moment(),
       groupEventsByMonths: [],
       currentMonthEvents: [],
       currentMonth: Moment(),
     }
+    console.log('sosososi', this)
   }
 
   _changeFocus = (focus) => {
-    //TODO: trigger event data
-    this.setState({focus, monthOffset: 0});
-    this._nextStage();
+    this.setState({focus, monthOffset: 0})
+    this._nextStage()
   }
 
   _stageText = () => {
     if (this.state.stage === DAY_SELECTOR) {
-      return this.state.focus.format('MMMM YYYY');
+      return this.state.focus.format('MMMM YYYY')
     } else {
-      return this.state.focus.format('YYYY');
+      return this.state.focus.format('YYYY')
     }
   }
 
@@ -71,7 +70,6 @@ export default class Calendar extends Component {
     if (this.state.stage === MONTH_SELECTOR) {
       this.setState({stage: YEAR_SELECTOR})
     }
-    // LayoutAnimation.easeInEaseOut();
   }
 
   _nextStage = () => {
@@ -81,112 +79,25 @@ export default class Calendar extends Component {
     if (this.state.stage === YEAR_SELECTOR) {
       this.setState({stage: MONTH_SELECTOR})
     }
-    // LayoutAnimation.easeInEaseOut();
-  }
-
-  _currentMonthEvents(date) {
-    this.setState({currentMonth: date})
-
-    let currentMonthEvents = this._getInitialMonthEv(this.state.currentMonth, this.state.groupEventsByMonths)
-
-    if (currentMonthEvents !== undefined) {
-
-      this.setState({currentMonthEvents: currentMonthEvents})
-
-      // Get days events
-      let daysWithEvent = this._getDayWithEvents(currentMonthEvents)
-
-      this.props.onMonthChange(date, currentMonthEvents, daysWithEvent)
-      this.setState({daysWithEvent: daysWithEvent})
-    } else {
-      // this.props.onDayChange(date)
-      this.props.onMonthChange(date)
-      this.setState({daysWithEvent: []})
-      this.setState({currentMonthEvents: []})
-    }
   }
 
   _previousMonth = () => {
     let date = this.state.currentMonth.subtract(1, 'month')
 
-    this._currentMonthEvents(date)
-    
-    this.setState({monthOffset: -1});
+    this.props.onMonthChange(date)
+    this.setState({monthOffset: -1})
   }
 
   _nextMonth = () => {
     let date = this.state.currentMonth.add(1, 'month')
 
-    this._currentMonthEvents(date)
-    
+    this.props.onMonthChange(date)
     this.setState({monthOffset: 1})
   }
 
-  _getDayWithEvents(currentMonthEvents) {
-    let daysWithEvent
-
-    let days = currentMonthEvents.map((res) => {
-      return parseInt(Moment(res.date, 'YYYY-MM-DD').format('D'))
-    })
-
-    return daysWithEvent = days.filter((elem, index, self) => {
-      return index == self.indexOf(elem)
-    })
-  }
-
-  _getInitialMonthEv(date, groupEventsByMonths) {
-    let currentMonthEvents
-
-    for (var key in groupEventsByMonths) {
-      if (date.format('YYYY-MM') == key) {
-        return currentMonthEvents = groupEventsByMonths[key]
-      }
-    }
-  }
-
-  _getEvByMonths() {
-    let groupEventsByMonths
-
-    return groupEventsByMonths = _.groupBy(this.props.annualEvents, (res) => {
-      return res.date.substring(0, 7)
-    })
-  }
-
-  _getDayEvents(date) {
-    let dayEvents = []
-    let formatedDay = Moment(date).format('YYYY-MM-DD')
-    this.state.currentMonthEvents.map((event) => {
-      if (event.date === formatedDay) {
-        dayEvents.push(event)
-      }
-    })
-
-    return dayEvents
-  }
   _onDayChange(date) {
-    let formatedDay = Moment(date).format('YYYY-MM-DD')
-
-    // Get day events
-    let dayEvents = this._getDayEvents(date)
-
-    this.props.onDayChange(date, dayEvents)
+    this.props.onDayChange(date)
     this.setState({date: date})
-  }
-
-  componentDidMount() {
-    let currentMonthEvents
-    let groupEventsByMonths
-    if (this.props.annualEvents.length != 0) {
-      // Get event group by months
-      groupEventsByMonths = this._getEvByMonths()
-      this.setState({groupEventsByMonths: groupEventsByMonths})
-      //Get initial month events
-      currentMonthEvents = this._getInitialMonthEv(this.state.currentMonth, groupEventsByMonths)
-      this.setState({currentMonthEvents: currentMonthEvents})
-      // Get days with events
-      let daysWithEvent = this._getDayWithEvents(currentMonthEvents)
-      this.setState({daysWithEvent: daysWithEvent})
-    }
   }
 
   render() {
@@ -196,15 +107,70 @@ export default class Calendar extends Component {
     const previousMonthValid = this.props.minDate.diff(Moment(previousMonth).endOf('month'), 'seconds') <= 0;
     const nextMonth = Moment(this.state.focus).add(1, 'month');
     const nextMonthValid = this.props.maxDate.diff(Moment(nextMonth).startOf('month'), 'seconds') >= 0;
+    const dayDelectorComponent = (
+      <DaySelector
+        ref='dayselector'
+        focus={this.state.focus}
+        selected={this.state.date}
+        onFocus={this._changeFocus}
+        onDayChange={(date) => this._onDayChange(date)}
+        monthOffset={this.state.monthOffset}
+        minDate={this.props.minDate}
+        maxDate={this.props.maxDate}
+        // Transfer the corresponding styling properties.
+        dayHeaderView={this.props.dayHeaderView}
+        dayHeaderText={this.props.dayHeaderText}
+        dayRowView={this.props.dayRowView}
+        dayView={this.props.dayView}
+        daySelectedView={this.props.daySelectedView}
+        dayText={this.props.dayText}
+        dayTodayText={this.props.dayTodayText}
+        daySelectedText={this.props.daySelectedText}
+        dayDisabledText={this.props.dayDisabledText}
+        dayPainted={this.props.dayPainted}
+        />
+    )
 
+    const monthSelectorComponent = (
+      <MonthSelector
+        focus={this.state.focus}
+        onFocus={this._changeFocus}
+        minDate={this.props.minDate}
+        maxDate={this.props.maxDate}
+        // Styling properties
+        monthText={this.props.monthText}
+        monthDisabledText={this.props.monthDisabledText}
+        />
+    )
+
+    const yearSelectorComponent = (
+      <YearSelector
+        focus={this.state.focus}
+        onFocus={this._changeFocus}
+        minDate={this.props.minDate}
+        maxDate={this.props.maxDate}
+        // Styling properties
+        minimumTrackTintColor={this.props.yearMinTintColor}
+        maximumTrackTintColor={this.props.yearMaxTintColor}
+        yearSlider={this.props.yearSlider}
+        yearText={this.props.yearText}
+        />
+    )
+
+    const calendar = () => {
+      if (this.state.stage === DAY_SELECTOR) {
+        return dayDelectorComponent
+      } else if (this.state.stage === MONTH_SELECTOR) {
+        return monthSelectorComponent
+      } else if (this.state.stage === YEAR_SELECTOR) {
+        return yearSelectorComponent
+      }
+
+      return null
+    }
     return (
-      <View style={[{
-        minWidth: 300,
-        // Wrapper view default style.
-      },this.props.style]}>
-        <View style={{
-          flexDirection: 'row',
-        }}>
+      <View style={[{minWidth: 300}, this.props.style]}>
+        <View style={{flexDirection: 'row'}}>
           <View style={[styles.barView, this.props.barView]}>
             { this.props.showArrows && this.state.stage === DAY_SELECTOR && previousMonthValid ?
               <TouchableHighlight
@@ -240,55 +206,7 @@ export default class Calendar extends Component {
         </View>
         <View
           style={[styles.stageWrapper, this.props.stageView]}>
-          {
-            this.state.stage === DAY_SELECTOR ?
-            <DaySelector
-              ref='dayselector'
-              focus={this.state.focus}
-              selected={this.state.date}
-              onFocus={this._changeFocus}
-              onDayChange={(date) => this._onDayChange(date)}
-              monthOffset={this.state.monthOffset}
-              minDate={this.props.minDate}
-              maxDate={this.props.maxDate}
-              // Control properties
-              slideThreshold={this.props.slideThreshold}
-              // Transfer the corresponding styling properties.
-              dayHeaderView={this.props.dayHeaderView}
-              dayHeaderText={this.props.dayHeaderText}
-              dayRowView={this.props.dayRowView}
-              dayView={this.props.dayView}
-              daySelectedView={this.props.daySelectedView}
-              dayText={this.props.dayText}
-              dayTodayText={this.props.dayTodayText}
-              daySelectedText={this.props.daySelectedText}
-              dayDisabledText={this.props.dayDisabledText}
-              dayPainted={this.props.dayPainted}
-              /> :
-            this.state.stage === MONTH_SELECTOR ?
-            <MonthSelector
-              focus={this.state.focus}
-              onFocus={this._changeFocus}
-              minDate={this.props.minDate}
-              maxDate={this.props.maxDate}
-              // Styling properties
-              monthText={this.props.monthText}
-              monthDisabledText={this.props.monthDisabledText}
-              /> :
-            this.state.stage === YEAR_SELECTOR ?
-            <YearSelector
-              focus={this.state.focus}
-              onFocus={this._changeFocus}
-              minDate={this.props.minDate}
-              maxDate={this.props.maxDate}
-              // Styling properties
-              minimumTrackTintColor={this.props.yearMinTintColor}
-              maximumTrackTintColor={this.props.yearMaxTintColor}
-              yearSlider={this.props.yearSlider}
-              yearText={this.props.yearText}
-              /> :
-            null
-          }
+          {calendar()}
         </View>
       </View>
     )
@@ -296,20 +214,17 @@ export default class Calendar extends Component {
 }
 Calendar.defaultProps = {
   minDate: Moment(),
-  maxDate: Moment().add(10, 'years'),
+  maxDate: Moment().add(2, 'years'),
   startStage: DAY_SELECTOR,
   showArrows: false,
 }
 
 Calendar.propTypes = {
   // The core properties.
-  annualEvents: React.PropTypes.array,
   selected: React.PropTypes.any,
   onDayChange: React.PropTypes.any,
   onMonthChange: React.PropTypes.any,
-  slideThreshold: React.PropTypes.number,
   dayPainted: React.PropTypes.array,
-  groupedByMonths: React.PropTypes.array,
   // Minimum and maximum date.
   minDate: React.PropTypes.any,
   maxDate: React.PropTypes.any,
